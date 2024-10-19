14fd59964dbe4a5779b2228d138e5a138baeec8e
IsabellaFerreir4 <if1823107@gmail.com>
2024-10-19 20:33:38 -0300

    prova

---
+++ Migrations/20241019212244_Migracao1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF.Exemplo6.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracao1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
